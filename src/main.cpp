d3acca34dd73cac8e61390b72b4c61d68705d46b
Andres Flores Bañuelos <159966414+Andres-Flores-Ba@users.noreply.github.com>
2024-04-17 00:22:51 +0000

    Version 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
+#include <thread>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+using namespace std;
+using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    
+    Element lienzo = hbox({
+        spinner(21,3) | bold
+    });
+
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Fit(lienzo)
+        );
+
+    Render(pantalla,lienzo);
+    pantalla.Print();
+    std::this_thread::sleep_for(0.1s);
+
+
     return 0;
 }
