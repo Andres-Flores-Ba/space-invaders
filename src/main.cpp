bd0bbb42a74998b97b6de6add9815512dbd47860
Andres Flores Bañuelos <159966414+Andres-Flores-Ba@users.noreply.github.com>
2024-04-19 23:30:22 +0000

    Version 4

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,45 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 
+
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    Element lienzo = hbox({
-        spinner(21,3) | bold
-    });
+    fstream archivo;
+    archivo.open("./assets/images/canon.txt");
+
+    string canon;
+    archivo>> canon;
+    archivo.close();
+
+    archivo.open("./assets/images/alien.txt");
+
+    string alien;
+    archivo>> alien;
+    archivo.close();
+
+    int fotograma = 0;
+    while (true)
+    {
+    fotograma++;
+    // Element personaje = 
+    Element lienzo = hbox({spinner(21,fotograma) | bold | color(Color::Aquamarine1Bis) | bgcolor(Color::BlueViolet)});
 
     Screen pantalla = Screen::Create(
         Dimension::Full(),
         Dimension::Fit(lienzo)
         );
 
+
+
     Render(pantalla,lienzo);
     pantalla.Print();
-    std::this_thread::sleep_for(0.1s);
+    cout<<pantalla.ResetPosition();
 
+    this_thread::sleep_for(0.1s);
+    }
 
     return 0;
 }
